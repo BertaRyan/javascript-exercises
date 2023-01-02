0cb6427a1e488cf37307d8ed2555908916ab1362
Ryan Berta <ryanberta1234@gmail.com>
2023-01-01 20:38:43 -0800

    finished palindromes.js

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,17 @@
-const palindromes = function () {
+const palindromes = function (str) {
+let re = /[^A-Za-z0–9]/g;
+let lowRegStr = str.toLowerCase().replace(re,"");
+let reverseStr = lowRegStr.split("").reverse().join('');
 
+if(lowRegStr === reverseStr){
+    return true;
+} else {
+    return false;
+}
 };
+palindromes("A man, A plan, A Canal. Panama")
+
+
 
 // Do not edit below this line
 module.exports = palindromes;
