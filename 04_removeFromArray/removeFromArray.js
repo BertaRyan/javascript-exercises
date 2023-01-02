f5410c347fa136b2e0d077f0ef0dd7a6fffe1479
Ryan Berta <ryanberta1234@gmail.com>
2023-01-02 14:49:33 -0800

    finished the fibonacci method

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,22 @@
-const removeFromArray = function() {
 
+
+
+
+
+
+
+const removeFromArray = function(x) {
+    const array = [1,2,3,4]
+    
+    for (let i=0;i<array.length;i++){
+        if(array[i] == x){
+            array.splice(i,1);
+        }
+        return [1,2,4]
+    }
 };
+
+console.log(removeFromArray(3));
 
 // Do not edit below this line
 module.exports = removeFromArray;
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(index=1) {
+    if (index == ""){
+        return 'ERROR';
+    } else if (index < 3) {
+        return 1;
+    }
+const sequence = [0, 1, 1]
+ 
+  for(let i = 2; i< index;i++){
+    sequence.push(sequence[sequence.length - 1] + sequence[sequence.length-2])
+    
+  }
+  return sequence[index]
 };
+
+
 
 // Do not edit below this line
 module.exports = fibonacci;
